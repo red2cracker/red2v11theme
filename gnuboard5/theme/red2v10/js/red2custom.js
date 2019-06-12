9ae6aaf3ec68176abc31c2b701c1448ca2cd2309
leezche <red2cracker@gmail.com>
2019-06-12 09:36:02 +0900

    로고 텍스트 font-weight: 600, 드롭다운 메뉴에 마우스 올리면 매뉴명 컬러, 배경색 바뀜.

--- gnuboard5/theme/red2v10/js/red2custom.js
+++ gnuboard5/theme/red2v10/js/red2custom.js
@@ -1,1 +1,27 @@
-$(function(){$('div.main-menu').hover(function(){$('.main-link').css("color","red")},function(){$('.main-link').css("color","white")});$('div.comm-menu').hover(function(){$('.comm-link').css("color","red")},function(){$('.comm-link').css("color","white")});$('div.down-menu').hover(function(){$('.down-link').css("color","red")},function(){$('.down-link').css("color","white")})});
+/*
+
+$(function(){
+
+//$('.test_jquery').css('background-color', 'red');
+
+$('div.main-menu').hover(function() {
+  $('.main-link').css("color", "red");
+}, function(){
+  $('.main-link').css("color", "white");
+});
+
+$('div.comm-menu').hover(function() {
+  $('.comm-link').css("color", "red");
+}, function(){
+  $('.comm-link').css("color", "white");
+});
+
+$('div.down-menu').hover(function() {
+  $('.down-link').css("color", "red");
+}, function(){
+  $('.down-link').css("color", "white");
+});
+
+});
+
+*/
